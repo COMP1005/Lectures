85c77e0c5071c70ccbf8e1a99313f838680e5447
maxvillev <34053958+maxvillev@users.noreply.github.com>
2024-03-25 02:11:36 +0800

    Add files via upload

---
+++ Lecture5/listcomp2.py
@@ -0,0 +1,17 @@
+def double(x):
+    return x*2
+
+list1 = [3,4,5]
+multiplied = [item*3 for item in list1]
+
+# [9,12,15]
+print(multiplied)
+
+doubled = [double(x) for x in range(10)]
+# [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+print(doubled)
+
+doubled = [double(x) for x in ["a","b","c"]]
+# [“aa”, “bb”, “cc”]
+print(doubled)
+
